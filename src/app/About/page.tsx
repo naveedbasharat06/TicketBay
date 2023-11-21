bcd80dd0acc15ed0fe1dc5443271a41165597ee2
asim <asimmazari9@gmail.com>
2023-11-22 03:39:36 +0500

    intial commit

---
+++ src/app/About/page.tsx
@@ -0,0 +1,9 @@
+import React,{useState} from 'react'
+const About=()=> {
+  // const[test,setTest]=useState(1)
+  return (
+    <div>About</div>
+  )
+}
+
+export default About 
---
+++ src/app/constants.ts
@@ -0,0 +1,10 @@
+export const CUSTOMMENU = [
+  {
+    id: 1,
+    menu: "Home",
+  },
+  {
+    id: 2,
+    menu: "About",
+  },
+];
